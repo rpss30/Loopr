520ec71f5c000628410ffd36195faa6ca8476009
Rishavpreet Singh <rishavpreetsingh@gmail.com>
2026-08-07 12:34:44 -0700

    clean default playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,6 @@ const webBaseUrl = process.env.LOOPR_E2E_WEB_BASE_URL ?? 'http://127.0.0.1:8082'
 
 export default defineConfig({
   testDir: './e2e',
-  testIgnore: 'dotnet-api/**',
   timeout: 30_000,
   expect: {
     timeout: 5_000,
