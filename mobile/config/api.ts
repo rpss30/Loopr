ff05683c27c15e811c0bcfd1a76e42b595a6a861
Rishavpreet Singh <rishavpreetsingh@gmail.com>
2026-08-07 09:25:00 -0700

    point mobile defaults at dotnet api

--- mobile/config/api.ts
+++ mobile/config/api.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_API_BASE_URL = 'http://localhost:3001';
+export const DEFAULT_API_BASE_URL = 'http://localhost:5101';
 
 type ApiEnvironment = {
   EXPO_PUBLIC_LOOPR_API_BASE_URL?: string;
